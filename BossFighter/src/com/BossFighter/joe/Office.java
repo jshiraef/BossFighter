09400bcf80b795ce9c516362a93a0c4f4e3d7337
jshiraef <joeshiraef@gmail.com>
2014-07-14 21:57:10 -0700

    made tiles using paint.net. Used tile map editor to build a mock background. edited Tile class to read the tile map

--- BossFighter/src/com/BossFighter/joe/Office.java
+++ BossFighter/src/com/BossFighter/joe/Office.java
@@ -1,5 +1,6 @@
 package com.BossFighter.joe;
 
+import java.awt.Graphics;
 import java.awt.Rectangle;
 
 import org.newdawn.slick.SlickException;
@@ -12,7 +13,7 @@ public class Office {
 	
 	public BossBackground[][] bg = new BossBackground[width][height];
 	
-	public final String Dpath= "res/World/room_";
+	public final String Dpath= "res/world/room_";
 	
 	public String path = Dpath;
 	
@@ -35,6 +36,51 @@ public class Office {
 		}
 	}
 	
+	loadWorld();
+	
    }
+	
+	public void loadWorld() {
+		int background = map.getLayerIndex("background");
+		int solids = map.getLayerIndex("collision");
+		int item = map.getLayerIndex("object");
+		
+		for(int x = 0; x < bg.length; x++) {
+			for (int y = 0; y < bg[0].length; y++){
+				
+				
+				//background
+				if(map.getTileId(x, y, background) == 1){
+					bg[x][y].id = Tile.floor;
+				}
+				if(map.getTileId(x, y, background) == 2){
+					bg[x][y].id = Tile.desk;
+				}
+				if(map.getTileId(x, y, background) == 3){
+					bg[x][y].id = Tile.carpet;
+				}
+				
+				//items
+			
+			}
+		}
+			
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g, int camX, int camY, int renderX, int renderY	) {
+		
+		for (int x = (camX/Tile.size); x < (camX/Tile.size) + renderX; x++) {
+			for (int y = (camY/Tile.size); y < (camY/Tile.size) + renderY; y++) {
+				if (x >= 0 && y <=0 && x < width && y < height) {
+					bg[x][y].render(g);
+				}
+			}
+		}
+		
+	}
 
 }
